9a78f1af97f9d9fb57ac4140d464ed37af7e7f42
LeoLcy <406131001@qq.com>
2016-02-26 18:15:13 +0800

    增加LoginVM

---
+++ Leo.Blog.ViewModel/SysModule/LoginVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leo.Blog.ViewModel.SysModule
+{
+    /// <summary>
+    /// 登录
+    /// </summary>
+    public class LoginVM
+    {
+        /// <summary>
+        /// 账号
+        /// </summary>
+        public string UserId { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// 验证码
+        /// </summary>
+        public string ValidCode { get; set; }
+    }
+    /// <summary>
+    /// 存放登录成功后个人信息，包括所属角色，组织，模块
+    /// </summary>
+    public class UserAccountVM
+    {
+
+    }
+}
