78a5f31a29b95c73e342101504823193044e9af3
LeoLcy <406131001@qq.com>
2016-02-28 23:19:38 +0800

    增加

--- Leo.Blog.BLL/SysModule/Sys_Account_B.cs
+++ Leo.Blog.BLL/SysModule/Sys_Account_B.cs
@@ -1,19 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Leo.Blog.DAL.SysModule;
 using Leo.Blog.Model;
+using Leo.Blog.ViewModel.SysModule;
 
 namespace Leo.Blog.BLL.SysModule
 {
     public class Sys_Account_B
     {
-        private Sys_Account_D _sysAccountD = new Sys_Account_D();
-        public void Login(string userId, string password)
+        private  Sys_Account_D _sysAccountD = new Sys_Account_D();
+        public UserAccountVM Login(string userId, string password)
         {
-            return;
+            Sys_Account account = _sysAccountD.FindSingle(m => m.Account == userId&&m.Password==password);
+            if (account == null)
+            {
+                throw new Exception("账号或密码错误");
+            }
+            //账号密码正确，查找各种权限组织
+            UserAccountVM accountVM = new UserAccountVM();
+
+            return accountVM;
+
         }
     }
 }
